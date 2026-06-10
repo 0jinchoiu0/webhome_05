1d905a21d15a96705301de1fa8b8836419ce7766
0jinchoiu0 <jinchoiu@gmail.com>
2026-06-10 09:32:19 +0800

    Set Vite base path for GitHub Pages project deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/webhome_05/',
   plugins: [
     vue(),
     vueDevTools(),
